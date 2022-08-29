a9564986099efcf1f85f1729440a26c015dd7771
Mitchell Keenan <mk@mitchkeenan.com>
2022-08-29 09:00:26 -0300

    Add rewrite config
    Don't rewrite static requests
    
    Don't rewrite static files #2
    
    Try not to rewrite static
    
    Fix favicon hopefully

--- next.config.js
+++ next.config.js
@@ -1,7 +1,40 @@
+// See example: https://vercel.com/guides/can-i-redirect-from-a-subdomain-to-a-subpath
+const rewrites = async () => {
+	return {
+		beforeFiles: [
+			// if the host is `getpaid.vercel.mitchkeenan.com`,
+			// this rewrite will be applied
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "getpaid.vercel.mitchkeenan.com",
+					},
+				],
+				destination: "/payroll/:path*",
+			},
+			{
+				source: "/payroll/_next/static/:path*",
+				destination: "/_next/static/:path*",
+			},
+			{
+				source: "/payroll/public/:path*",
+				destination: "/public/:path*",
+			},
+			{
+				source: "/payroll/:favicon(favicon.*)",
+				destination: "/:favicon*",
+			},
+		],
+	};
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	rewrites,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
